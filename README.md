1c71b234121283d00bccc9d45fdfc2187da5635c
Kwe <Kweayon@gmail.com>
2023-08-14 23:34:54 -0500

    first go lesson

---
+++ README.md
@@ -0,0 +1,7 @@
+# GoLang Tutorial
+
+Learn GoLang with me step by step.
+
+How to initiate a GoLang module:
+
+go mod init example/tutorial
---
+++ lesson1/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type Emplyee struct {
+	id     int
+	fname  string
+	lname  string
+	salary float32
+}
+
+func spacedPrint(str string) {
+	fmt.Println("\n" + str + "\n")
+}
+
+func main() {
+	intArr := [3]int{1, 2, 3}
+
+	emp1 := Emplyee{id: 1101, fname: "Kweayon", lname: "Clark", salary: 130000}
+
+	spacedPrint("Hello Universe.")
+	fmt.Printf("%v\n\n", intArr)
+	fmt.Printf("%v\n\n", emp1)
+
+}
+
+// fmt
+//   - a package part of the standard library
+//   - used to format and print strings

63d1cf376a886e0a90b97ef0380378246372be56
Kwe <Kweayon@gmail.com>
2023-09-03 14:13:03 -0500

    update: first database connection with go!

--- 3-lesson/example_postgres.go
+++ 3-lesson/example_postgres.go
@@ -1,13 +1,55 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	// "database/sql"
+
 	// "net/http"
 	// "log"
 	_ "github.com/lib/pq"
 )
 
-func main() {
-	fmt.Println("Postgres Example!")
+var db *sql.DB
+
+// This function will make a connection to the database only once.
+func init() {
+
+	var err error
+	connStr := "postgres://postgres:password@localhost/db_1?sslmode=disable"
+	db, err = sql.Open("postgres", connStr)
+
+	CheckError(err)
+
+	// check if database is alive, if err is not null then the connection is not alive.
+	err = db.Ping()
+
+	CheckError(err)
+
+	// close database
+	defer db.Close()
+
+	// check alive connection
+	err = db.Ping()
+
+	CheckError(err)
+
+	fmt.Println("\nThe database is connected.\n ")
+
 }
+
+func CheckError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	// fmt.Println("from main function")
+
+}
+
+// init function
+
+//   - initialization function, runs at the initial start of your application
+//   - init can be declared multiple times, executed in the order defined

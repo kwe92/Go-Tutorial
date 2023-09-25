d0ab03d5893555d03305f8b27d4c9c29a734c165
Kwe <Kweayon@gmail.com>
2023-09-25 15:57:11 -0500

    update: type parameters

---
+++ tour_of_go_lessons/generics00/type+parameters.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+// GetIndex compares x to all elements in slice, returns index of x if exists or -1
+func GetIndex[T comparable](slice []T, x T) int {
+
+	for index, value := range slice {
+		if value == x {
+			return index
+		}
+	}
+	return -1
+}
+
+func main() {
+	// both strings and number implement the comparable interfac
+
+	intSlice := []int{0, 3, 6, 9, 12, 15, 18, 21, 24}
+
+	strSlice := []string{"welcome", "the", "void"}
+
+	fmt.Println(GetIndex(intSlice, 9))
+
+	fmt.Println(GetIndex(strSlice, "Universe"))
+
+}
+
+// Go Type Parameters
+
+//   - allows Go functions to work with multiple types
+//   - type parameters appear between the function name and function parameters
+//     placed between brackets
+//   - parameterized types with constraints
+
+// comparable built-in constraint
+
+//   - an interface that is implemented by all comparable types such as:
+//       (booleans, strings, numbers, collections of comparable types, struct with comparable type fields)
+//   - can only be used as a type parameter
+//   - allows the use of the equality and inequality operators == && !=
+//     for comparason of parameterized types
+//   - incorrect types are caught at compile-time

f2e21e539702b46e3019e6c0dd44ee807c68153a
Kwe <Kweayon@gmail.com>
2023-09-25 12:18:27 -0500

    updated comments

--- tour_of_go_lessons/reader03/rot13_reader_exercise.go
+++ tour_of_go_lessons/reader03/rot13_reader_exercise.go
@@ -1,3 +1,9 @@
+// Goal
+
+// Implement a rot13Reader that implements io.Reader
+// reads from an io.Reader, modifying the stream by applying
+// rot13 substitution cipher to all alphabetical characters.
+
 package main
 
 import (
@@ -10,7 +16,7 @@ type rot13Reader struct {
 	r io.Reader
 }
 
-// TODO: Refactor to shorten algorithm
+// TODO: Refactor to shorten algorithm implementation and use modulo 26
 
 func (rot *rot13Reader) Read(b []byte) (n int, err error) {
 
@@ -20,7 +26,7 @@ func (rot *rot13Reader) Read(b []byte) (n int, err error) {
 
 		if b[i] >= []byte("A")[0] && b[i] <= []byte("Z")[0] {
 
-			places := (b[i] + 13) % 92
+			places := (b[i] + 13) % 91
 
 			if places < 65 {
 
@@ -60,3 +66,26 @@ func main() {
 
 	io.Copy(os.Stdout, &r)
 }
+
+// Rotate by 13 Places
+
+//   - an implementation of the Cesear Substitution Cipher
+//     that replaces each letter in the plain text with a letter 13 positions down
+//     starting from the beginning if Z is reached
+
+// Caesar Cipher
+
+//   - An ancient cryptographic substitution cipher
+//   - Ciphers are algorithms that perform encryption and decryption
+//   - substitution ciphers encrypt plain text by replacing the letters
+//     with other letters number and symbols
+//   - if the plaintext is viewed as a sequence of bits the
+//     subtitution evolves replacing the plain text bit pattern with the cipher text bit pattern
+
+// Caesar Cipher Algorithm Expressed Mathematically
+
+//   - Encryption:
+//       E = (p + k) % 26
+
+//   - Decryption
+//       D = (p - k) % 26

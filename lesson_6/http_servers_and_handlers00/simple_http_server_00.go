4b74082b1dbc3511a3467be0e54fa9703d38f82d
Kwe <Kweayon@gmail.com>
2023-10-05 16:14:16 -0500

    update: comment update

--- lesson_6/http_servers_and_handlers00/simple_http_server_00.go
+++ lesson_6/http_servers_and_handlers00/simple_http_server_00.go
@@ -12,27 +12,38 @@ const (
 
 func main() {
 
-	// declare and initialize a ServeMux instance
+	// declare and initialize ServeMux instance
 	mux := http.NewServeMux()
 
-	// register handler to path
-	mux.Handle("/", &home{})
+	// register handler to pattern
+	mux.Handle("/", &homeHandler{})
 
 	fmt.Println("Server has started!")
 
-	// start HTTP server on port 8080 using the ServeMux instance as a handler
+	// start HTTP server on port 8080
 	log.Fatal(http.ListenAndServe(Address, mux))
 
 }
 
-type home struct {
+// homeHandler implements http.Handler interface
+type homeHandler struct {
 }
 
-func (h *home) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Add("Content-Type", "text/plain")
+
+	w.WriteHeader(200)
+
 	w.Write([]byte("Form is form, Emptiness is emptiness."))
+
 }
 
-// ServerMux
+// HTTP Server | Concept
+
+//   - a service that listens for incoming HTTP Requests and returns an HTTP Response
+
+// ServeMux | Struct
 
 //   - a struct that is an HTTP multiplexer allowing the creation of HTTP servers
 //   - ServerMux has methods to register handlers to paths `URLs` and
@@ -40,24 +51,30 @@ func (h *home) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 //   - if a request URL matches a path the registered handler is executed
 //   - routes are matched to paths in O(n) `linear-time`
 
-// http.NewServeMux
+// http.NewServeMux | Exported Function
 
 //   - allocates a new ServeMux struct in memory
-//     as your own instance
+//     which can be used as an instance instead of using the defaultServeMux
 
-// ServeMux.Handle(path, handler)
+// Handle | Method | ServeMux
 
-//   - a method that registers a route handler for a given path `patten / URL`
+//   - a method that registers a route handler to a given pattern `path / URL`
 
-// ServeHTTP method
+// http.Handler | Interface | http package
+
+//   - an interface that defines a handler object
+//   - has one method signature ServeHTTP
+//   - used to create custom handlers
+
+// ServeHTTP | Method | http.Handler
 
 //   - required to implement the http.Handler interface
-//   - ServeHTTP is responsible for writing headers and content to the http.ResponseWriter
+//   - ServeHTTP writes headers and content to the http.ResponseWriter
 //   - takes two arguments:
 //       * http.ResponseWriter
 //       * pointer to a http.Request struct
 
-// http.ListenAndServe
+// http.ListenAndServe | Exported Function
 
 //   - Starts an HTTP server that listens on a TCP network address with a specified port and
 //     calls Serve on the handler to handle all incoming HTTP requests on that address

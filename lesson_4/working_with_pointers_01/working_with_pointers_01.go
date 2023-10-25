be06cecbdd6f37c5dcca8d17fe6f3037d6611175
Kwe <Kweayon@gmail.com>
2023-10-25 11:31:24 -0500

    working with pointers and structs

---
+++ lesson_4/working_with_pointers_01/working_with_pointers_01.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	firstName  string
+	middleName *string
+	lastName   string
+}
+
+func main() {
+	person := Person{
+		firstName:  "Kweayon",
+		middleName: returnPointer[string]("LaCharles"),
+		lastName:   "Clark",
+	}
+
+	fmt.Println(person)
+}
+
+// returnPointer: a generic function that returns a pointer to a value of any type.
+func returnPointer[T any](value T) *T {
+	return &value
+}
+
+// struct: Pointer Types For Fields
+
+//   - if a field has a type of pointer type then the assigned value must be a pointer type
+
+// Primitive Literal Pointers
+
+//   - primitives can not be prefixed with & address operator
+//   - you must assign the value to a variable and point to that variable or return a pointer from a function

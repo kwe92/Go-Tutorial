51c586fd0efa4ab1f46ede948804f542db913532
Kwe <Kweayon@gmail.com>
2023-10-28 00:07:15 -0500

    update: expanded lesson and comments

--- tour_of_go_lessons/hash_maps00/hash_map_lesson_00.go
+++ tour_of_go_lessons/hash_maps00/hash_map_lesson_00.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // nil map, can't be assigned keys or values
-var m map[string]interface{}
+var m map[string]any
 
 func main() {
 
@@ -14,29 +14,29 @@ func main() {
 
 	fmt.Println("\nThe Answer to The Universe: ", m["Answer"])
 
-	updateIfExists(&m, "Answer", 48)
+	updateIfExists(m, "Answer", 48)
 
 	fmt.Println("\n\nThe Answer to The Universe: ", m["Answer"])
 
-	updateIfExists(&m, "I_Dont_Exist", 42)
+	updateIfExists(m, "I_Dont_Exist", 42)
 
-	deleteIfExists(&m, "Answer")
+	deleteIfExists(m, "Answer")
 
 	fmt.Printf("\n\nThe Answer to The Universe: %v\n\n", m["Answer"])
 
 }
 
 // updateIfExists updates a value at a specified key if it exists
-func updateIfExists(m *map[string]interface{}, key string, value interface{}) bool {
+func updateIfExists[T any](m map[string]T, key string, value T) bool {
 
 	var isPresent bool
 
-	previousValue, isPresent := (*m)[key]
+	previousValue, isPresent := m[key]
 
 	if isPresent {
 		fmt.Printf("\nupdate from: %v", previousValue)
-		(*m)[key] = value
-		fmt.Printf("\n\nupdate to: %v", (*m)[key])
+		m[key] = value
+		fmt.Printf("\n\nupdate to: %v", m[key])
 
 		return isPresent
 	}
@@ -45,17 +45,23 @@ func updateIfExists(m *map[string]interface{}, key string, value interface{}) bo
 }
 
 // deleteIfExists updates a value at a specified key if it exists.
-func deleteIfExists(m *map[string]interface{}, key string) bool {
+func deleteIfExists(m map[string]interface{}, key string) bool {
 
 	var isPresent bool
 
-	_, isPresent = (*m)[key]
+	_, isPresent = m[key]
 
 	if isPresent {
-		delete((*m), key)
+		delete(m, key)
 		fmt.Printf("\n\nDeleted value at key: %v", key)
 		return isPresent
 	}
 	fmt.Printf("\nThe key: %v is not present in the hashMap", key)
 	return isPresent
 }
+
+// map
+
+//   - maps are a key value pair data structure
+//   - there are many ways to implement a map but in GO maps
+//     are implemented with a hash map

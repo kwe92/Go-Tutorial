59b5c3aa562b3d2ff5732cc6f86143e4b25d49f9
Kwe <Kweayon@gmail.com>
2023-11-05 12:39:05 -0600

    update: expanded lesson and comments

--- concurrency/channels/channels03/select_statement.go
+++ concurrency/channels/channels03/select_statement.go
@@ -2,35 +2,26 @@ package main
 
 import "fmt"
 
-//TODO: understand how the value is being passed around
-
-//! Explain this code works
-
 func main() {
+
+	n := 10
 
 	ch := make(chan int)
 
 	done := make(chan struct{})
 
 	go func() {
-		for i := 0; i < 10; i++ {
-			// channel read
+		for i := 0; i < n; i++ {
+			// reading from the channel n times
 			fmt.Println(<-ch)
 		}
-		// channel done pattern
+
+		// channel done pattern | initialize the done channel so it is no longer nil
 		done <- struct{}{}
+
 	}()
 
-	for i := 0; i < 10; i++ {
-		// channel write
-		ch <- (i)
-
-	}
-
-	// ! explain more complicated examples
-
-	// fibonacci(ch, done)
-	// addOne(ch, done)
+	addOne(ch, done)
 
 }
 
@@ -40,8 +31,12 @@ func addOne(ch chan int, done chan struct{}) {
 
 	for {
 		select {
+
+		// write to channel
 		case ch <- x:
 			x++
+
+		// if done is not nil print done and return out of the loop
 		case <-done:
 			fmt.Println("done")
 			return
@@ -65,6 +60,9 @@ func fibonacci(ch chan int, done chan struct{}) {
 
 // Select Statement
 
-//   - gives a go-routine the ability to wait on multiple communication operations
+//   - gives a goroutine the ability to wait on multiple communication operations
 //   - the select blocks until one of the cases can run and then runs that case
 //   - if multiple cases are ready then one is executed at random
+//   - cases with a nil value are skipped
+
+// Done Pattern

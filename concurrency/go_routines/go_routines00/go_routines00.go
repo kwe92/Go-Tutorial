248716b3fae52aa122a7f92d7e8c0643b47ae20b
Kwe <Kweayon@gmail.com>
2023-11-06 13:17:20 -0600

    update: expanded lesson and comments

--- concurrency/go_routines/go_routines00/go_routines00.go
+++ concurrency/go_routines/go_routines00/go_routines00.go
@@ -24,14 +24,20 @@ func main() {
 
 // Concurency
 
-//   - multiple computations happening at the same time
-//   - a process is broken into individual components
+//   - multiple computations happening simultaneously
+//   - breaking a process into individual components `threads`
+
+// Concurrency Definition by Google io
+
+//   - the composition of independently executing computations
+//   - can be viewed as a model for software construction in which software is constructed in a way
+//     where clean code is written and interacts well with the real world
 
 // Process
 
 //   - an instance of a program that was given partitioned resources by the operating system during the virtualization process
 //   - processes run in isolation and do not have access to another processes resources
-//   - a processes are composed of one or more threads
+//   - processes are composed of one or more threads
 
 // Threads
 
@@ -50,8 +56,13 @@ func main() {
 
 // goroutines
 
+//   - independently executing functions launched by the go statement
 //   - lightweight processes managed by the GO runtime
-//   - they can be seen as extremly cheap threads
+//   - they can be seen as extremely cheap threads
+//   - goroutines do not make the caller wait
+//   - goroutines have their own call stack and they are elastic `can grow and shrink`
+//   - a single threaded program can have multiple goroutines
+//?  - goroutines are multiplexed onto threads as needed
 
 // Launching a Function as a goroutine
 
@@ -59,15 +70,17 @@ func main() {
 //   - to indicate a function will run a concurrent process the `go` keyword preceeds a function invocation
 //   - any function can be prefixed with the go keyword but it is idiomatic to wrap the function in a closure
 
-// When a Go Program Starts | change the name later
+// Main Function: The Initial goroutine
 
 //   - when a Go program starts the Go Runtime launches a single goroutine and creates a number of threads
-//   - the initial goroutine and all other goroutines are automatically assigned to the threads
-//     that the Go Runtime created by the Go Runtime Scheduler `similar to the operating system process scheduler but cheaper`
+//   - the Go Runtime Scheduler automatically assigns the initial goroutine `main function` and all subsequent
+//     goroutines to the threads created by the Go Runtime
+//   - The Go Runtime Scheduler is similar to the operating system process scheduler but cheaper
+//     as you are not creating an operating system level resource
 
 // goroutine Address Space
 
-//   - Go routines run in the same address space
+//   - goroutines run in the same address space
 //   - Memory access that is shared must be synchronized
 //   - to synchronize memory you can use primitives in GO or the sync package
 

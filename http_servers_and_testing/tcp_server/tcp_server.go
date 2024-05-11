69423286ede7b2cdf8e7c969b47729d2dc51324b
Kwe <Kweayon@gmail.com>
2024-05-10 21:03:29 -0500

    feat: expanded comments on Sockets, Socket Components, How Sockets Work, How Sockets Are Bound, How Server Side Sockets Work

--- http_servers_and_testing/tcp_server/tcp_server.go
+++ http_servers_and_testing/tcp_server/tcp_server.go
@@ -9,12 +9,12 @@ import (
 // Creating a Echo Server Using Sockets
 
 var (
-	PROTOCOL = "tcp" // use the Transmission Control Protocol
+	PROTOCOL = "tcp" // communication protocol: Transmission Control Protocol
 	PORT     = ":8080"
 )
 
 func main() {
-	// create a new socket bound to port 8080
+	// create a new socket of type TCP bound to port 8080
 	// listening for an incoming connection from a client
 	serverListener, err := net.Listen(PROTOCOL, PORT)
 
@@ -53,7 +53,7 @@ func handleClientConnection(ClientConn net.Conn) {
 	_, err := ClientConn.Read(buff)
 
 	if err != nil {
-		fmt.Printf("error reeading data from client connection in handleClientConnection: %s\n", err.Error())
+		fmt.Printf("error reading data from client connection in handleClientConnection: %s\n", err.Error())
 		return
 	}
 
@@ -66,8 +66,67 @@ func handleClientConnection(ClientConn net.Conn) {
 
 //   - one endpoint of a two-way communication link between two programs running on a network
 
-//   - a socket is bound to a port number allowing the TCP layer to identify
-//     the application that the data is destined to be sent to for both client requests and server responses
+//   - a socket is bound to a port number
+
+//   - a socket is uniquely identified by the composition of IP address and port number
+
+//   - a sockets unique identifier ensures that communication is happening between the server and the correct client
+
+// Components of a Socket
+
+//   - Each socket consists of:
+
+//       - IP address: Identifies the device on the network.
+
+//       - Port number: Specifies the specific application or service on that device.
+
+//       - Protocol: Defines the rules of communication (e.g., TCP (ordered, reliable and consistent) or UDP (potentially unordered, sent in packets that can droped)).
+
+// How Sockets Work
+
+//   - Socket Creation:
+
+//       - upon creation a socket is given a protocol type typically TCP or UDP
+
+//   - Binding
+
+//       - a socket is bound to a specified port and IP address an the machine in which it was created
+
+//   - Connecting TCP Sockets
+
+//       - Connections are estiablished with the remote socket (server socket)
+
+//   - Listening on TCP Sockets
+
+//       - servers listen on TCP sockets for incoming connection requests
+
+//   - Data Transfer
+
+//       - data can be transmitted as a stream of bytes between both applications
+
+//   - Closing Sockets
+
+//       - when the communication between both parties is finished sockets should be closed as to not leak resources
+
+// Ports vs Sockets:
+
+//   - Purpose:
+
+//       - Ports identify applications
+
+//       - Sockets enable communication between applications
+
+//   - Type:
+
+//       - Ports are numerical identifiers
+
+//       - Sockets are complex structures with multiple elements
+
+//   - Scope:
+
+//       - Ports are local to a computer
+
+//       - Sockets involve two endpoints on potentially different devices
 
 // Identifying TCP Connections
 
@@ -77,3 +136,22 @@ func handleClientConnection(ClientConn net.Conn) {
 // Socket programming is an important skill for network programming
 
 // building powerful network applications with socket programming and the Client-Server Model
+
+// Server Side Socket Binding
+
+//   - servers run on a specific machine identified by its host name
+
+//   - sockets are then bound to a port number specified by some listener or mux implementation
+
+//   - servers listen to the socket for incoming connection requests from clients
+
+//   - the server then determines how to deal with the request
+
+//   - if a server accepts a clients request to connect then a new socket is bound to the same
+//     local port as the client requesting services
+
+//   - the remote endpoint (socket) is set to the IP address and port of the client
+
+// TODO: this can definitely be worded better
+//   - the afromentioned process allows servers to concurrently process requests by always listen for incoming client
+//     requests to connect on the original listening socket
